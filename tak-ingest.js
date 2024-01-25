c24ea283087db1bd9d5673ea79c0d64aa8ebf0f0
Dave Conway-Jones <dceejay@gmail.com>
2024-01-25 13:58:24 +0000

    Better handle protobuf, bump libs

--- tak-ingest.js
+++ tak-ingest.js
@@ -20,37 +20,48 @@ module.exports = function(RED) {
         var global = this.context().global;
         const parser = new XMLParser(fastXmlOptions);
 
+        var handleProtoBuf = function(msg) {
+            protobuf.load(path.join(__dirname,"proto/takmessage.proto"), function (err, root) {
+                if (err) { node.error(err); }
+                var m = root.lookupType("atakmap.commoncommo.protobuf.v1.TakMessage");
+                var m3;
+                try {
+                    var m2 = m.decode(msg.payload);
+                    m3 = m.toObject(m2, { longs:String, enums:String, bytes:String } ).cotEvent;
+                }
+                catch(e) { node.error("Protobuf decode "+e,msg); return; }
+
+                // Add any unique ID/callsigns to some global variables just in case it's useful for later.
+                if (m3.detail?.contact?.callsign && m3.uid) {
+                    var a = global.get("_takgatewaycs") || {};
+                    var c = a[m3.detail.contact.callsign];
+                    a[m3.detail.contact.callsign] = m3.uid;
+                    global.set("_takgatewaycs", a);
+                    var b = global.get("_takgatewayid") || {};
+                    if (c) { delete b[c]; }
+                    b[m3.uid] = m3.detail.contact.callsign;
+                    global.set("_takgatewayid", b);
+                }
+
+                node.send({
+                    topic: m3.type,
+                    payload: { cotEvent: m3 }
+                });
+            });
+        }
+
         node.on("input",function(msg) {
             if (Buffer.isBuffer(msg.payload)) {
+                // handle UDP packet
                 if (msg.payload[0] === 191 && msg.payload[2] === 191) {
                     var head = msg.payload[1];
                     msg.payload = msg.payload.slice(3);
+                    // type 1 = protobuf
                     if (head === 1) {
-                        protobuf.load(path.join(__dirname,"proto/takmessage.proto"), function (err, root) {
-                            if (err) { node.error(err); }
-                            var m = root.lookupType("atakmap.commoncommo.protobuf.v1.TakMessage");
-                            var m2 = m.decode(msg.payload);
-                            var m3 = m.toObject(m2, { longs:String, enums:String, bytes:String } ).cotEvent;
-
-                            // Add any unique ID/callsigns to some global variables just in case it's useful for later.
-                            if (m3.detail?.contact?.callsign && m3.uid) {
-                                var a = global.get("_takgatewaycs") || {};
-                                var c = a[m3.detail.contact.callsign];
-                                a[m3.detail.contact.callsign] = m3.uid;
-                                global.set("_takgatewaycs", a);
-                                var b = global.get("_takgatewayid") || {};
-                                if (c) { delete b[c]; }
-                                b[m3.uid] = m3.detail.contact.callsign;
-                                global.set("_takgatewayid", b);
-                            }
-
-                            node.send({
-                                topic: m3.type,
-                                payload: { cotEvent: m3 }
-                            });
-                        });
+                        handleProtoBuf(msg);
                         return;
                     }
+                    // type 0 = plain text
                     else if (head === 0) {
                         msg.payload = msg.payload.toString();
                     }
@@ -59,6 +70,13 @@ module.exports = function(RED) {
                         return;
                     }
                 }
+                // ideally need to check if tcp and streaming protobuf...
+                // else if (msg.payload[0] === 191 && !msg.hasOwnProperty("fromip")) {
+                //     var l = msg.payload[1] + msg.payload[2] * 256;
+                //     msg.payload = msg.payload.slice(3,l+3);
+                //     handleProtoBuf(msg);
+                //     return;
+                // }
                 else {
                     node.error("Unknown buffer type",msg);
                     return;
@@ -68,6 +86,7 @@ module.exports = function(RED) {
                 node.error("Input is not a string.",msg);
                 return;
             }
+            msg.payload = msg.payload.trim();
             if (msg.payload.indexOf("<event") !== 0) {
                 if (msg.payload.trim().length > 0 && msg.payload !== '</event>') { // ignore blank lines
                     node.error("Input is not an XML event string.",msg);
